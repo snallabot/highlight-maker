e306e04934457fdf4571201288727e2fb349c272
Sahith Nallapareddy <sahith.reddy@gmail.com>
2024-02-17 13:54:58 -0500

    adding cv to only get scoreboard, ready for OCR

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # highlight-maker
-snallabot highlight maker for Maddem
+
+snallabot highlight maker for Madden 24.
+
+## Developing
+
+I am using pyenv with version 3.12.2. I created my own virtual env and installed the packages in `requirements.txt`. The other key piece is `youtube-dl` which you can find https://github.com/ytdl-org/youtube-dl
+
+Download Test Video
+```
+youtube-dl -o test-videos/test-gameplay.mp4 https://www.youtube.com/watch\?v\=oEmk-gIOoDU
+```
+
+this is a gameplay of mine that I have been using for testing.
+
+if you want to create your own virtual env:
+
+```
+# if you need to install python 3.12.2
+# pyenv install 3.12.2
+pyenv virtualenv 3.12.2 highlight-maker
+pyenv activate highlight-maker
+pip install -r requirements.txt
+```
+
+Then you can run the file
+
+```
+python highlight.py
+```
--- highlight.py
+++ highlight.py
@@ -1,10 +1,19 @@
 from __future__ import print_function
 import cv2 as cv
 import argparse
-import easyocr
 
-capture = cv.VideoCapture(cv.samples.findFileOrKeep("./cards2.mp4"))
+capture = cv.VideoCapture(cv.samples.findFileOrKeep("./test-videos/test-gameplay.mp4"))
 fps = capture.get(cv.CAP_PROP_FPS)
+width = capture.get(cv.CAP_PROP_FRAME_WIDTH)
+height = capture.get(cv.CAP_PROP_FRAME_HEIGHT)
+startWidth = 570/2000
+endWidth = 1434/2000
+startHeight = 56/1090
+endHeight = 118/1090
+
+start = (int(startWidth * width), int(startHeight * height))
+end = (int(endWidth * width), int(endHeight * height))
+
 print(fps)
 # fourcc = cv.VideoWriter_fourcc(*'XVID')
 # out = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
@@ -22,16 +31,18 @@ while True:
         # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         # noise = cv.medianBlur(gray, 3)
         # thresh = cv.threshold(noise, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
-        reader = easyocr.Reader(['en'])
-        result = reader.readtext(frame, paragraph="false")
-        print(result)
-    frames.append(frame)
+        # reader = easyocr.Reader(['en'])
+        # result = reader.readtext(frame, paragraph="false")
+        # print(result)
+        scoreboard = frame[start[1]:end[1], start[0]:end[0]]
+        gray = cv.cvtColor(scoreboard, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(gray, 127, 255, 0)
+        cv.bitwise_not(thresh, thresh)
+        cv.imshow('frame', thresh)
+        cv.waitKey(1)
     currentFrame += 1
     if currentFrame == fps:
-        seconds.append(frames)
-        frames = []
         currentFrame = 0
-        print(len(seconds))
     
     # cv.imshow('Frame', frame)
     # cv.imshow('FG Mask', fgMask)
